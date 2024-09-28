44f826afaf0170d8d49861e2c007c34984d65f4b
zf <zdenek.frydryn@gmail.com>
2024-09-28 15:04:30 +0200

    Menu 1.0 - basic bootstrap

--- index.php
+++ index.php
@@ -9,14 +9,39 @@
 </head>
 <body>
 <header>
-    <menu>
-        <ul>
-            <li>Username</li>
-            <li>Datum</li>
-            <li><a href="loginpage.php">Přihlásit</a></li>
-            <li><a href="registerpage.php">Registrace</a></li>
-        </ul>
-    </menu>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">D.O.T.A.Z</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Všechny články</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Věda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Technika</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Článek, autor, téma, ..." aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Vyhledat</button>
+                </form>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="loginpage.php">Přihlásit se</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registerpage.php">Registrovat se</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
 
 <main>
