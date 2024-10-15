e62a7f30245bda7af7d5e62feb464795028f888a
zf <zdenek.frydryn@gmail.com>
2024-10-15 22:14:29 +0200

    Uprava Layoutu podle UI designu
    zatim pouze index a article. Pridany tlacitka, linky vedou na clanky.
    Barvy odpovidaji navrhu.
    
    TODO:Menu je potreba predelat + tvorba loga. Vytvorit footer.

--- include/header.php
+++ include/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-blue fixed-top">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">D.O.T.A.Z</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
