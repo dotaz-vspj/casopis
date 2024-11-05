34e86d01eab92ccd6c0556fd79bf6ec971d25536
zf <zf-pentest@gmail.com>
2024-11-05 16:44:32 +0100

    myid promena je v session

--- ostatni/index.php
+++ ostatni/index.php
@@ -1,7 +1,7 @@
 <?php 
 //ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 session_start();
-$myID=0; ?>
+$myID=$_SESSION['user']['id']; ?>
 <?php
 $EditionID=htmlentities($_GET['id']);
 
