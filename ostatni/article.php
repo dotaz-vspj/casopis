4dfcdc083716340a2c4be95d0d57ecb40c8472d4
zf <zdenek.frydryn@gmail.com>
2024-11-17 17:30:43 +0100

    Jeste pridany article.php

--- ostatni/article.php
+++ ostatni/article.php
@@ -6,7 +6,7 @@
 <?php include 'include/session_open.php'; ?>
 <?php
 $ArticleID=htmlentities($_GET['id']);
-
+$category=htmlentities($_GET['cat']);
 if (!is_numeric($ArticleID)) {Header("location:index.php");die;}
 include 'include/db.php'; 
 $sql = "SELECT A.*, case when A.Status=5 then E.Published else C.descr end Published FROM `RSP_ARTICLE` A ".
@@ -18,7 +18,7 @@ if ($result->rowCount() == 0) {Header("location:index.php");die;}
 $A=$result->fetchObject();
 ?>
 <?php include 'include/header.php'; ?>
-<div class="container-fluid mb-5">
+<div class="container-fluid mb-5" style="padding-top: 90px;">
     <div class="row justify-content-center">
         <div class="col-sm-8">
             <div class="jumbotron mb-5">
@@ -42,29 +42,34 @@ $A=$result->fetchObject();
                 <div class="col-sm-3">
                     Autor: <span class="author">
 <?php 
-$sql = "SELECT GROUP_CONCAT(CONCAT(LastName, ', ', CASE WHEN TitleF IS NULL THEN '' ELSE CONCAT(TitleF, ' ') END, FirstName, CASE WHEN TitleP IS NULL THEN '' ELSE CONCAT(',', TitleP) END) ORDER BY LastName SEPARATOR '<br/>\n') FROM `RSP_ARTICLE_ROLE` R LEFT JOIN `RSP_USER` U ON R.Person=U.ID WHERE R.Role=24 AND R.Article={$ArticleID}";
+$sql = 'SELECT GROUP_CONCAT(CONCAT(LastName,", ",case when TitleF is null then "" else CONCAT(TitleF," ") end,FirstName,case when TitleP is null then "" else CONCAT(",",TitleP) end) ORDER BY LastName SEPARATOR "<br/>\n") FROM `RSP_ARTICLE_ROLE` R left join `RSP_USER` U on R.Person=U.ID where R.Role=24 and R.Article='.$ArticleID;
 $result = $conn->query($sql);
 echo $result->fetch()[0];
 ?>
                     </span>
+<?php if ($A->ActiveVersion!=null) {?>
+                    <a href="include/ajax/getDocument?id=<?php echo "{$A->ID}";?>">
+                       <img src="<?php echo "{$img_dir}";?>download.png" height=50 alt="Soubor ke stažení">
+                    </a>
+<?php } ?>
                 </div>
             </div>
             <div class="row justify-content-between mb-5">
                 <div class="col-sm-2">
                     <a href="<?php 
-$sql = "SELECT MAX(ID) FROM `RSP_ARTICLE` where ID<{$ArticleID} and hasAccess({$myID},ID)";
+$sql = 'SELECT MAX(ID) FROM `RSP_ARTICLE` where ID<'.$ArticleID." and hasAccess(".$myID.",ID)";
 $result = $conn->query($sql);$nID=$result->fetch()[0];
-echo ($nID == "") ? 'javascript:void(0)" style="color:gray; ' : "article.php?id={$nID}";
+echo (($nID == "")?'javascript:void(0)" style="color:gray; ':'article.php?id='.$nID.'&cat=' . htmlentities($category));
 ?>" class="prev text-center box-btn btn-size-mid">Předchozí</a>
                 </div>
                 <div class="col-sm-2">
-                    <a href="discusion.php?id=<?php echo htmlentities($ArticleID) ?>" class="text-center box-btn btn-size-mid">Diskuze</a>
+                    <a href="discusion.php?id=<?php echo htmlentities($ArticleID) . "&cat=" . $category; ?>" class="text-center box-btn btn-size-mid">Diskuze</a>
                 </div>
                 <div class="col-sm-2">
                     <a href="<?php 
-$sql = "SELECT MIN(ID) FROM `RSP_ARTICLE` where ID>{$ArticleID} and hasAccess({$myID},ID)";
+$sql = 'SELECT MIN(ID) FROM `RSP_ARTICLE` where ID>'.$ArticleID." and hasAccess(".$myID.",ID)";
 $result = $conn->query($sql);$nID=$result->fetch()[0];
-echo ($nID == "")?'javascript:void(0)" style="color:gray; ':'article.php?id='.$nID;
+echo (($nID == "")?'javascript:void(0)" style="color:gray; ':'article.php?id='.$nID.'&cat=' . htmlentities($category));
 ?>" class="next text-center box-btn btn-size-mid">Následující</a>
                 </div>
             </div>
