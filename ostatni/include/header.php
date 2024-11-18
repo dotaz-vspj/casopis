b60c73dc804550a4b3104bc32a28788621a2de04
zf <zdenek.frydryn@gmail.com>
2024-11-18 18:03:51 +0100

    Oprava linku na admin

--- ostatni/include/header.php
+++ ostatni/include/header.php
@@ -42,7 +42,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <?php if (isset($_SESSION['user'])): ?>
                             <li class="nav-item">
-                                <a class="nav-link" href="include/profile.php">Profil</a>
+                                <a class="nav-link" href="profile.php">Administrace</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="include/logout.php">Odhlásit se</a>
