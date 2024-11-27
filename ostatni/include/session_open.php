bd7247ab170bb2dbc50a85a2addc2b47707aebba
zf <zdenek.frydryn@gmail.com>
2024-11-27 18:22:48 +0100

    Odstraneni debug hlasek

--- ostatni/include/session_open.php
+++ ostatni/include/session_open.php
@@ -1,5 +1,5 @@
 <?php 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 session_start();
 $myID=0;if (isset($_SESSION['user'])&&isset($_SESSION['user']['id'])) $myID=$_SESSION['user']['id'];
 
